84b25ab021dd1bb46b5075a32ffd7ddb45b17e27
Zhenzhen Liu <zhenzhen1108@hotmail.com>
2023-11-07 17:56:42 +1100

    Update QFfunction.py

--- QFfunction.py
+++ QFfunction.py
@@ -2,7 +2,12 @@ import qianfan
 import re
 import streamlit as st
 
-
+def list_synonyms(synonyms: list[str]):
+    """
+    Show a list of synonyms to the user.
+    """
+    return synonyms
+    
 def add_numbers(a: int, b: int):
     """
     This function adds two numbers.
@@ -13,7 +18,7 @@ def say_hello(name: str):
     """
     This function greets the user.
     """
-    return f"Hello, {name}!"
+    return f"你好, {name}!"
 
 
 def eb_call(prompt, round):
@@ -73,7 +78,7 @@ def eb_call(prompt, round):
     return response
 
 chat_comp = qianfan.ChatCompletion(ak="LrQvpiE6f4npsUwEvPL9vEWF", sk="CHTwBMVM0DlwyoGTLGEyRviBdctgOv4G")
-prompt = "Can you add up 42069420 and 6969420？### Hey how are you？###"
+prompt = "这两个数加起来是多少，42069420 和 6969420？### 我叫ZZ，你好###"
 prompt_list = re.split(r"###", prompt)
 
 for questions in prompt_list:

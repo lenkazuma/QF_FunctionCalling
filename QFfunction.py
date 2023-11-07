1c0babf4280a40a4e078fc15547fedb359b57edd
Zhenzhen Liu <zhenzhen1108@hotmail.com>
2023-11-07 17:39:17 +1100

    Update QFfunction.py

--- QFfunction.py
+++ QFfunction.py
@@ -1,6 +1,18 @@
 import qianfan
 import re
-import streamlit as st
+
+def add_numbers(a: int, b: int):
+    """
+    This function adds two numbers.
+    """
+    return a + b
+
+def say_hello(name: str):
+    """
+    This function greets the user.
+    """
+    return f"Hello, {name}!"
+
 
 def eb_call(prompt, round):
     print(prompt)
@@ -15,67 +27,42 @@ def eb_call(prompt, round):
             temperature=0.000000001,
             functions=[
                 {
-                    "name": "delivery_inquiry",
-                    "description": "查询商品",
+                    "name": "add_numbers",
+                    "description": "This function adds two numbers.",
                     "parameters": {
                         "type": "object",
                         "properties": {
-                            "location": {
-                                "type": "string",
-                                "description": "地址信息，包括街道、门牌号、城市、省份等信息"
-                                },
-                            "expect_price": {
-                                "type": "int",
-                                "description": "期望的价格"
-                                }
+                            "a": {
+                                "type": "integer",
+                                "description": ""
                             },
-                        "required": ["location"]
+                            "b": {
+                                "type": "integer",
+                                "description": ""
+                            }
                         },
-                    "responses": {
+                        "required": [
+                            "a",
+                            "b"
+                        ]
+                    }
+                },
+                {
+                    "name": "say_hello",
+                    "description": "This function greets the user.",
+                    "parameters": {
                         "type": "object",
                         "properties": {
-                            "id": {
+                            "name": {
                                 "type": "string",
-                                "description": "商品id"
-                                },
-                            "price": {
-                                "type": "int",
-                                "description": "商品价格"
-                                },
-                            "food": {
-                                "type": "string",
-                                "description": "商品名称"
-                                },
-                            },
+                                "description": ""
+                            }
                         },
-                 },
-                 {
-                        "name": "delivery_order",
-                        "description": "外卖下单",
-                        "parameters": {
-                            "type": "object",
-                            "properties": {
-                                "id": {
-                                    "type": "string",
-                                    "description": "商品id"
-                                    },
-                                "food": {
-                                    "type": "string",
-                                    "description": "商品名称"
-                                    },
-                                },
-                            "required": ["id"]
-                            },
-                        "responses": {
-                            "type": "object",
-                            "properties": {
-                                "result": {
-                                    "type": "string",
-                                    "description": "是否下单成功"
-                                    },
-                                }
-                            },
-                   }
+                        "required": [
+                            "name"
+                        ]
+                    }
+                }
                  ]
             )
 
@@ -84,7 +71,7 @@ def eb_call(prompt, round):
     return response
 
 chat_comp = qianfan.ChatCompletion(ak="LrQvpiE6f4npsUwEvPL9vEWF", sk="CHTwBMVM0DlwyoGTLGEyRviBdctgOv4G")
-prompt = "百度大厦-北京海淀区上地十街10号的附近，午餐有哪些推荐？###能不能帮我点一个20元以内的？###"
+prompt = "你能计算一下42069420 + 6969420等于多少吗？###嗨你怎么样？###"
 prompt_list = re.split(r"###", prompt)
 
 for questions in prompt_list:

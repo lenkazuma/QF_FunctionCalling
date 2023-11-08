07f247af024e142ac71119e1d8c8cbd035d6b898
Zhenzhen <zhenzhen1108@hotmail.com>
2023-11-08 15:19:21 +1100

    test

--- QFfunction.py
+++ QFfunction.py
@@ -146,14 +146,9 @@ def eb_call(prompt, round):
                         },
                         "required": ["location"]
                     }
-            },
-                ],
-        examples= [
-        {"role":"user","content":"What is the weather like in Boston?"},
-        {"role": "assistant", "content": "null", "function_call": {"name": "get_current_weather", "arguments": "{ \"location\": \"Boston\"}"}},
-        {"role": "function", "name": "get_current_weather", "content": "{\"temperature\": \"25\", \"unit\": \"centigrade\", \"description\": \"cloud\"}"}
-        ],
-        stream="true"
+            }
+            ]
+
     )
 
 
@@ -174,31 +169,6 @@ employee_list_df={}
 for questions in prompt_list:
     response = eb_call(questions, round)
     st.write(response['result'])
-    #response_message = response["choices"][0]["message"]
 
-    if response.get('function_call'):
-        # Which function call was invoked
-        function_called = response_message['function_call']['name']
-        
-        # Extracting the arguments
-        function_args  = json.loads(response_message['function_call']['arguments'])
-        
-        # Function names
-        available_functions = {
-            "say_hello": say_hello,
-            "add_numbers": add_numbers,
-            "mutiply_numbers": mutiply_numbers,
-            "extract_employee_info": extract_employee_info
-        }
-        
-        fuction_to_call = available_functions[function_called]
-        response_message = fuction_to_call(*list(function_args .values()))
-        
-    else:
-        #response_message = response_message['content']
-        print("error")
-    
-    #print(f"\nSample#{i+1}\n")
-    #print(response_message)
 
-    st.write(employee_list_df)
+st.write(employee_list_df)

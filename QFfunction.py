57219d592c87c665673f4247f3d01a0bb3aa2f55
Zhenzhen <zhenzhen1108@hotmail.com>
2023-11-08 14:11:47 +1100

    test df

--- QFfunction.py
+++ QFfunction.py
@@ -1,6 +1,10 @@
 import qianfan
 import re
 import streamlit as st
+import pandas as pd
+import numpy as np
+np.random.seed(123)
+
 
 def list_synonyms(synonyms: list[str]):
     """
@@ -25,6 +29,16 @@ def mutiply_numbers(a: int, b: int):
     This function multiplies two numbers.
     """
     return a + b
+
+
+def extract_employee_info(employee_list_df,name: str,department: str,certificate:str,id:int):
+    """
+    This function extracts the information of an employee and sort it into correct format.
+    """
+    new_row = {'姓名': name, '工号': id, '部门': department, '学历': certificate} 
+    employee_list_df.append(new_row, ignore_index=True)
+
+    return f"员工信息表格已更新"
 
 def eb_call(prompt, round):
     st.write(prompt)
@@ -95,6 +109,32 @@ def eb_call(prompt, round):
                             "name"
                         ]
                     }
+                },
+                {
+                    'name': 'extract_employee_info',
+                    'description': 'Get the employee information from the body of the input text',
+                    'parameters': {
+                        'type': 'object',
+                        'properties': {
+                            'name': {
+                                'type': 'string',
+                                'description': 'Name of the person'
+                            },
+                            'department': {
+                                'type': 'string',
+                                'description': 'Department the employee belongs to.'
+                            },
+                            'certificate': {
+                                'type': 'string',
+                                'description': 'The qualification or certificate that the employee holds.'
+                            },
+                            'id': {
+                                'type': 'string',
+                                'description': 'The id number of the employee. '
+                            }
+                            
+                        }
+                    }
                 }
                  ]
             )
@@ -104,9 +144,17 @@ def eb_call(prompt, round):
     return response
 
 chat_comp = qianfan.ChatCompletion(ak="LrQvpiE6f4npsUwEvPL9vEWF", sk="CHTwBMVM0DlwyoGTLGEyRviBdctgOv4G")
-prompt = "这两个数加起来是多少，42069420 和 6969420？### 我叫Wenxin，你好### 请问23乘109是多少 ###"
-prompt_list = re.split(r"###", prompt)
+prompt1 = "这两个数加起来是多少，42069420 和 6969420？"
+prompt2 = "我叫Wenxin，你好"
+prompt3 = "请问23乘109是多少"
+prompt4 = "新入职员工李红在HR部门工作，她有研究生文凭。她的工号是918604"
+prompt5 = "张三的工号是114514，他本科毕业，在技术部工作。请添加一下他的信息。"
+prompt_list = [prompt1，prompt2，prompt3，prompt4，prompt5]
+
+employee_list_df={}
 
 for questions in prompt_list:
     response = eb_call(questions, round)
     st.write(response['result'])
+
+st.write(employee_list_df)
